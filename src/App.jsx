543f35d88063eece6cdb78138ba25aec9c3851d3
naiiiden <na4ko359@gmail.com>
2023-08-30 22:47:14 +0300

    products add quantity

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,67 @@
 import { useState } from "react"
 
 const App = () => {
+  const ulStyle = {
+    listStyle: 'none',
+    padding: 0,
+    display: 'flex',
+    flexWrap: 'wrap',
+  }
+
   const products = [
     {
       id: 1,
       name: 'Product 1',
       price: 11.99,
+      quantity: 5,
     },
     {
       id: 2,
       name: 'Product 2',
       price: 12.99,
+      quantity: 10,
     },
     {
       id: 3,
       name: 'Product 3',
       price: 13.99,
+      quantity: 2,
     },
     {
       id: 4,
       name: 'Product 4',
       price: 14.99,
+      quantity: 3,
     },
     {
       id: 5,
       name: 'Product 5',
       price: 15.99,
+      quantity: 20,
     },
     {
       id: 6,
       name: 'Product 6',
       price: 16.99,
+      quantity: 15,
     },
     {
       id: 7,
       name: 'Product 7',
       price: 17.99,
+      quantity: 0,
     },
     {
       id: 8,
       name: 'Product 8',
       price: 18.99,
+      quantity: 7,
     },
     {
       id: 9,
       name: 'Product 9',
       price: 19.99,
+      quantity: 6,
     },
   ]
 
@@ -64,29 +80,32 @@ const App = () => {
   return (
     <main>
       <h1>Cart functionality</h1>
-      <ul style={{
-        listStyle: 'none',
-        padding: 0,
-      }}>
+      <ul style={ulStyle}>
         {products.map((product) => (
           <li key={product.id} style={{
             background: 'darkcyan',
             width: 'fit-content',
             padding: '1rem',
-            margin: '1rem 0',
+            margin: '1rem',
           }}>
             <p style={{
               margin: '0 0 .5rem 0',
             }}>{product.name} - ${product.price}</p>
+            <p>Quantity: {product.quantity}</p>
             <button onClick={() => addItemToCart(product)}>Add to cart</button>
           </li>
         ))}
       </ul>
-      <h2>Cart</h2>
+      <h2>Cart Items</h2>
       {cartItems.length !== 0 && 
-        <ul>
+        <ul style={ulStyle}>
           {cartItems.map((item) => (
-            <li key={item.id}>
+            <li key={item.id} style={{
+              border: '1px solid red',
+              width: 'fit-content',
+              padding: '1rem',
+              margin: '1rem',
+            }}>
               <p>{item.name} - ${item.price}</p>
               <button onClick={() => removeItemFromCart(item)}>Remove from cart</button>
             </li>
