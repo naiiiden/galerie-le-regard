c31dd6075c84b10b6f47365d6e9a647b6a2d1e27
naiiiden <na4ko359@gmail.com>
2023-08-30 21:22:49 +0300

    render cart ul only if cartItems !== empty

--- src/App.jsx
+++ src/App.jsx
@@ -78,11 +78,13 @@ const App = () => {
         ))}
       </ul>
       <h2>Cart</h2>
-      <ul>
-        {cartItems?.map((item) => (
-          <li key={item.id}>{item.name} - ${item.price}</li>
-        ))}
-      </ul>
+      {cartItems.length !== 0 && 
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>{item.name} - ${item.price}</li>
+          ))}
+        </ul>
+      }
     </main>
   )
 }
