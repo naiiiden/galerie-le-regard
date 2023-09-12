26325a2b83b92e81c1a575739c33fa0e2df31d16
naiiiden <na4ko359@gmail.com>
2023-09-12 16:11:57 +0300

    singleproduct add add to cart button

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -1,9 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useCart } from '../../context/CartContext';
+import clearAlert from '../../helpers/clearAlert';
 
 const SingleProduct = () => {
   const { productName } = useParams();
-  const { cartState } = useCart(); 
+  const { cartState, dispatch } = useCart(); 
 
   const product = cartState.products.find((product) => product.name === productName);
 
@@ -14,10 +15,16 @@ const SingleProduct = () => {
             : 
             <div>
                 <div>
-                <h2>{product.name}</h2>
-                <p>Price: ${product.price}</p>
-                <p>Quantity: {product.quantity}</p>
-                <p>Category: {product.category}</p>
+                  <h2>{product.name}</h2>
+                  <p>Price: ${product.price}</p>
+                  <p>Quantity: {product.quantity}</p>
+                  <p>Category: {product.category}</p>
+                  <button
+                    onClick={() => 
+                      dispatch({ type: 'ADD_TO_CART', payload: product }, 
+                      clearAlert(dispatch)
+                    )}
+                  >Add to cart</button>
                 </div>
             </div>}
     </div>
