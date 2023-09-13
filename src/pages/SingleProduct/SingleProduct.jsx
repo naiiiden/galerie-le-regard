111b63a1dfefd86ab2df21b887277a35c12c113b
naiiiden <na4ko359@gmail.com>
2023-09-13 16:51:40 +0300

    singleproduct add quantity input

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -19,6 +19,16 @@ const SingleProduct = () => {
                   <p>Price: ${product.price}</p>
                   <p>Quantity: {product.quantity}</p>
                   <p>Category: {product.category}</p>
+                  <label htmlFor={`quantity-product-${product.id}`}>Quantity: </label>
+                  <input
+                    id={`quantity-product-${product.id}`}
+                    type='number'
+                    // TODO ADD VALUE
+                    // value={1}
+                    min={1}
+                    max={product.quantity}
+                  />
+                  <br/>
                   <button
                     onClick={() => 
                       dispatch({ type: 'ADD_TO_CART', payload: product }, 
