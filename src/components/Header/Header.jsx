8b213392932d193b39749ed1618b3f667ea14499
naiiiden <na4ko359@gmail.com>
2023-09-24 16:02:02 +0300

    click outside cart window

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,23 +12,26 @@ const Header = () => {
     openCart ? document.body.style.overflow = "hidden" : document.body.style.overflow = "";
 
     return (
-        <header style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            border: '1px solid red',
-            }}>
-            <h1>Cart functionality</h1>
-            <nav>
-                <NavLink to="/" style={{ marginRight: '1rem' }}>All Products</NavLink>
-                <button onClick={() => setOpenCart(true)} style={{ display: 'inline-flex', alignItems: 'center', gap: '.5rem' }}>
-                Cart
-                <TotalProductsInCart/>
-                </button>
-                <CartDropdown className={openCart ? 'cart-menu-open' : 'cart-menu-closed'} onClick={() => setOpenCart(false)}/>
-                {cartState.alert === null ? null : <p id="global-alert" key={Math.random()} aria-live="polite">{cartState.alert}</p>}
-            </nav>
-        </header>
+        <>
+            <div className={`click-outside ${openCart ? "close" : ""}`} onClick={() => setOpenCart(false)}></div>
+            <header style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                border: '1px solid red',
+                }}>
+                <h1>Cart functionality</h1>
+                <nav>
+                    <NavLink to="/" style={{ marginRight: '1rem' }}>All Products</NavLink>
+                    <button onClick={() => setOpenCart(true)} style={{ display: 'inline-flex', alignItems: 'center', gap: '.5rem' }}>
+                    Cart
+                    <TotalProductsInCart/>
+                    </button>
+                    <CartDropdown className={openCart ? 'cart-menu-open' : 'cart-menu-closed'} onClick={() => setOpenCart(false)}/>
+                    {cartState.alert === null ? null : <p id="global-alert" key={Math.random()} aria-live="polite">{cartState.alert}</p>}
+                </nav>
+            </header>
+        </>
     )
 }
 
