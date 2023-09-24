090d19979a77fe031df7cb092dcf7ce42f3c1408
naiiiden <na4ko359@gmail.com>
2023-09-24 20:33:10 +0300

    cart btn improvement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import TotalProductsInCart from "../TotalProductsInCart/TotalProductsInCart";
 import CartDropdown from "../CartDropdown/CartDropdown";
 import { useCart } from "../../context/CartContext";
 import { NavLink } from "react-router-dom";
@@ -23,9 +22,8 @@ const Header = () => {
                 <h1>Cart functionality</h1>
                 <nav>
                     <NavLink to="/" style={{ marginRight: '1rem' }}>All Products</NavLink>
-                    <button onClick={() => setOpenCart(true)} style={{ display: 'inline-flex', alignItems: 'center', gap: '.5rem' }}>
-                    Cart
-                    <TotalProductsInCart/>
+                    <button onClick={() => setOpenCart(true)} style={{ display: 'inline-flex', alignItems: 'center', gap: '.5rem' }} aria-label={openCart ? "Close Cart" : "Open Cart"} aria-live="polite">
+                        Cart
                     </button>
                     <CartDropdown className={openCart ? 'cart-menu-open' : 'cart-menu-closed'} onClick={() => setOpenCart(false)}/>
                     {cartState.alert === null ? null : <p id="global-alert" key={Math.random()} aria-live="polite">{cartState.alert}</p>}
