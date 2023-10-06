ac86624a0c62854c1b444b107181ed74399f50b9
naiiiden <na4ko359@gmail.com>
2023-10-06 23:15:43 +0300

    revert products.jsx

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react';
 import { UseCart } from '../../context/CartContext'
 import clearAlert from '../../helpers/clearAlert';
 import ProductItem from '../../components/ProductItem/ProductItem';
-import { useNavigate } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 const Products = ({ style }) => {
     const { cartState, dispatch } = UseCart();
     const [selectedCategory, setSelectedCategory] = useState('');
-    const navigate = useNavigate();
+    
     const categories = [...new Set(cartState.products.map(product => product.category))];
     const filteredProducts = selectedCategory === '' 
         ? cartState.products 
@@ -16,7 +15,7 @@ const Products = ({ style }) => {
 
     return (
         <div>
-            <select onChange={(e) => (setSelectedCategory(e.target.value), navigate(selectedCategory ? `/${selectedCategory}` : '/'))} value={selectedCategory} aria-label='Filter products by category'>
+            <select onChange={(e) => setSelectedCategory(e.target.value)} value={selectedCategory} aria-label='Filter products by category'>
                 <option value={''}>Filter by category</option>
                 {categories.map(category => 
                     <option key={category} value={category}>{category}</option>
