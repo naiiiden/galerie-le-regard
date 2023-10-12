094351c2a8a499577afee5460a33cd4ddd3c2ad8
naiiiden <na4ko359@gmail.com>
2023-10-12 19:47:04 +0300

    landing page text

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom"
 
 const Landing = () => {
     return (
-        <>
+        <div className="landing-page-container">
+            <p>Welcome to</p>
             <h1>Le Regard</h1>
-            <Link to="/paintings"><b>paintings</b></Link>
-        </>
+            <p>
+                A collection of the world&apos;s most renowned <Link to="/paintings">paintings</Link>
+            </p>
+        </div>
     )
 }
 
