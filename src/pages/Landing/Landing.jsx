6ffd6bb2ea3dd4b3d031ed2f0b1f210fd182c747
naiiiden <na4ko359@gmail.com>
2023-10-12 22:57:34 +0300

    landing page text improvements

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,11 +3,8 @@ import { Link } from "react-router-dom"
 const Landing = () => {
     return (
         <div className="landing-page-container">
-            <p>Welcome to</p>
             <h1>Le Regard</h1>
-            <p>
-                A collection of the world&apos;s most renowned <Link to="/paintings">paintings</Link>
-            </p>
+            <p>A collection of the world&apos;s most renowned <Link to="/paintings">paintings</Link>.</p>
         </div>
     )
 }
