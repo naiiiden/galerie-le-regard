36cc85f6c15ca15edfc87afa3b0afde3be309235
naiiiden <na4ko359@gmail.com>
2023-10-13 20:28:56 +0300

    landing page improvements

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,8 @@ const Landing = () => {
     return (
         <div className="landing-page-container">
             <h1>Le Regard</h1>
-            <p>A collection of the world&apos;s most renowned <Link to="/paintings">paintings</Link>.</p>
+            <p>A collection of the world&apos;s most renowned <Link to="/paintings">paintings</Link></p>
+            <p>(for peeps with pockets as deep as their love for art)</p>
         </div>
     )
 }
