d679ade32652b9ddc427c6c48eab52e34d5dcfce
naiiiden <na4ko359@gmail.com>
2023-10-15 17:04:25 +0300

    logo improvements?

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const Logo = () => {
   return (
     <NavLink to="/" className="logo">
-        <span aria-hidden="true">LR</span>  
+        <div aria-hidden="true"><span>LR</span></div>  
         <span>Galerie <br/>Le Regard</span>
     </NavLink>
   )
