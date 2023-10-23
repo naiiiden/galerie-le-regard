1aadd4786a4f3c9d5d97f6b71fd0a29eb6271169
naiiiden <na4ko359@gmail.com>
2023-10-23 22:20:48 +0300

    productitem add state effects

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -4,14 +4,9 @@ import { Link } from "react-router-dom"
 const ProductItem = ({ product, linkHref }) => {
   return (
     <li key={product.id}>
-        <Link to={linkHref} style={{ color: 'black' }}>
-          <img src={`/${product.image}`} alt='' style={{ width: "300px" }} title={product.name}/>
-          {/* <p style={{margin: '0 0 .5rem 0'}}>
-            <span>{product.name}</span>
-            <span>{product.price === null ? null : ` - ${product.price}`}</span>
-            </p>
-          {product.quantity === null ? null : <p>Quantity: {product.quantity}</p>}
-          <p>Category: {product.category}</p> */}
+        <Link to={linkHref} data-title={product.name}>
+          <img src={`/${product.image}`} alt='' style={{ width: "unset" }}/>
+          <p><span>{product.name}</span></p>
         </Link>
       </li>
   )
