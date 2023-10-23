ec03001cc009bf1778a14f9ba22cd3918f5e70fd
naiiiden <na4ko359@gmail.com>
2023-10-23 18:19:12 +0300

    cart fix long item name

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -21,22 +21,25 @@ const CartItem = ({ item, onQuantityChange, onRemove }) => {
       <img src={item.image} alt=""/>
       <div className="cart-item-text">
         <div>
-          <p>{item.name} - ${item.price}</p>
+          <p className="cart-item-name">{item.name}</p>
           <button onClick={() => onRemove(item)} aria-label={`Remove ${item.name} from your cart`}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width='15'>
               <path stroke="currentColor" strokeLinejoin="round" strokeWidth="1.5" d="m2 2 20 20m0-20L2 22"></path>
             </svg>
           </button>
         </div>
-        <input
-          aria-label="Product quantity"
-          id={`quantity-product-${item.id}`}
-          type='number'
-          value={item.quantity}
-          onChange={handleQuantityChange}
-          min='1'
-          max={item.availableQuantity}
-        />
+        <div className="cart-item-price">
+          <p>${item.price}</p>
+          <input
+            aria-label="Product quantity"
+            id={`quantity-product-${item.id}`}
+            type='number'
+            value={item.quantity}
+            onChange={handleQuantityChange}
+            min='1'
+            max={item.availableQuantity}
+          />
+        </div>
       </div>
     </li>
   )
