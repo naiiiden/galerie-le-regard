ca72d88c5eb9f85402d4cb76d07f1c3ea9365d1c
naiiiden <na4ko359@gmail.com>
2023-10-23 19:51:15 +0300

    cartitem layout improvements

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,6 +1,12 @@
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom";
+import { OpenCart } from "../../context/OpenCartContext";
+
 const CartItem = ({ item, onQuantityChange, onRemove }) => {
+
+  const { closeCart } = OpenCart();
+
   const handleQuantityChange = (event) => {
     const newQuantity = parseInt(event.target.value, 10);
     if (!isNaN(newQuantity) && newQuantity >= 1 && newQuantity <= item.availableQuantity) {
@@ -10,29 +16,29 @@ const CartItem = ({ item, onQuantityChange, onRemove }) => {
 
   return (
     <li key={item.id}>
-      <img src={`/${item.image}`} alt=""/>
-      <div className="cart-item-text">
-        <div>
-          <p className="text-overflow-fix">{item.name}</p>
-          <button onClick={() => onRemove(item)} aria-label={`Remove ${item.name} from your cart`}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width='15'>
-              <path stroke="currentColor" strokeLinejoin="round" strokeWidth="1.5" d="m2 2 20 20m0-20L2 22"></path>
-            </svg>
-          </button>
+        <img src={`/${item.image}`} alt=""/>
+        <div className="cart-item-text">
+          <div>
+            <Link to={`/paintings/${item.category}/${item.name}`} onClick={() => closeCart()} className="text-overflow-fix">{item.name}</Link>
+            <button onClick={() => onRemove(item)} aria-label={`Remove ${item.name} from your cart`}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width='15'>
+                <path stroke="currentColor" strokeLinejoin="round" strokeWidth="1.5" d="m2 2 20 20m0-20L2 22"></path>
+              </svg>
+            </button>
+          </div>
+          <div className="cart-item-price">
+            <p>${item.price}</p>
+            <input
+              aria-label="Product quantity"
+              id={`quantity-product-${item.id}`}
+              type='number'
+              value={item.quantity}
+              onChange={handleQuantityChange}
+              min='1'
+              max={item.availableQuantity}
+            />
+          </div>
         </div>
-        <div className="cart-item-price">
-          <p>${item.price}</p>
-          <input
-            aria-label="Product quantity"
-            id={`quantity-product-${item.id}`}
-            type='number'
-            value={item.quantity}
-            onChange={handleQuantityChange}
-            min='1'
-            max={item.availableQuantity}
-          />
-        </div>
-      </div>
     </li>
   )
 }
